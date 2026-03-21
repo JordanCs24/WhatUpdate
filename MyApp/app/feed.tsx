3c19a02b262c3722fe2763f6ef7744aa0a822db3
Jordan Sowell <155126975+JordanCs24@users.noreply.github.com>
2026-03-21 10:44:21 -0400

    Update feed.tsx
    added the variable UPDATES for the data. I'll add the back end later ofc.

--- MyApp/app/feed.tsx
+++ MyApp/app/feed.tsx
@@ -1,5 +1,38 @@
 import { Text, StyleSheet, ScrollView } from 'react-native';
 
+const UPDATES = [
+  {
+    id: '1',
+    game: 'Fortnite',
+    image: '#1a6b9a',
+    updates: {
+      newContent: ['New weapon: Plasma Rifle', 'New map location: Crystal Cave'],
+      bugFixes: ['Fixed shotgun spread bug', 'Fixed crash on lobby screen'],
+      balanceChanges: ['SMG damage reduced by 10%', 'Shield potion cooldown increased'],
+    }
+  },
+  {
+    id: '2',
+    game: 'Warzone',
+    image: '#7a3b1e',
+    updates: {
+      newContent: ['New operator skin: Ghost Recon', 'New game mode: Resurgence'],
+      bugFixes: ['Fixed parachute clipping bug'],
+      balanceChanges: ['Sniper rifle bullet velocity increased'],
+    }
+  },
+  {
+    id: '3',
+    game: 'Apex Legends',
+    image: '#8b0000',
+    updates: {
+      newContent: ['New legend: Spectre', 'New care package weapon'],
+      bugFixes: ['Fixed Wraith hitbox issue', 'Fixed audio bug in Kings Canyon'],
+      balanceChanges: ['Caustic gas damage increased', 'Wingman headshot multiplier reduced'],
+    }
+  },
+];
+
 export default function FeedScreen() {
   return (
     <ScrollView style={styles.container} contentContainerStyle={styles.content}>
