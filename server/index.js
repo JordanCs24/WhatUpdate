92ddf0e82a9350108756eb7a31c6adf9e359af1e
Jordan Sowell <155126975+JordanCs24@users.noreply.github.com>
2026-03-21 12:46:08 -0400

    5 files changed added the server
    Add Express server setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('WhatUpdate API is running!');
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected!');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('MongoDB connection error:', err);
+  });
