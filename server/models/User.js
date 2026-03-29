5806b7dbd9a56717491e8d4b41272cf30d9515c6
Jordan Sowell <155126975+JordanCs24@users.noreply.github.com>
2026-03-28 20:21:41 -0400

    Update User.js
    Added the validation for the different fields and making sure they hold in the database.

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true , minLength: 8},
+  games: [{ type: String }],
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
